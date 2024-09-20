3f5fed7fbff0cb889055635ef09e0b792dda2c64
Boris Nacev <bnachev77@gmail.com>
2024-09-20 15:54:48 +0200

    initial commite with a blade layout and nav

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.main')
+
+@section('title', 'Home')
+
+@section('content')
+
+    {{-- <p class="font_headings">Heading</p>
+    <p class="font_paragraphs">Text</p> --}}
+
+@endsection
---
+++ resources/views/layout/nav.blade.php
@@ -0,0 +1,49 @@
+<nav>
+    {{-- Top nav --}}
+    <div class="w-100 bg-black nav_shadow">
+        <div class="container py-2">
+            <div class="row align-items-center">
+                <div class="col-2">
+                    <a href=""
+                        class="h3 font_headings text-decoration-none h3 fw-bold text-white mb-0 d-inline">NEWSLETTER</a>
+                </div>
+                <div class="col-8">
+                    <input type="text" class="rounded-pill p-2 font_text border-8 text-white border-white bg-black"
+                        name="" id="" value="Search">
+                </div>
+                <div class="col-1">
+                    <p class="mb-0 font_text text-white">En/Mkd</p>
+                </div>
+                <div class="col-1">
+                    <a href="" class="text-white font_text text-decoration-none">E-shop</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- Bottom nav --}}
+    <div class="w-100 orange">
+        <div class="container py-1">
+            <div class="row">
+                <div class="col-7">
+                    <a href=""> <img style="width: 80px" src="{{ asset('images/logos/Крик-лого.png') }}"
+                            alt="Krik logo"></a>
+                </div>
+                <div class="col-5 d-flex align-items-center">
+                    <ul class="h5 d-flex w-100 font_text fw-bold nav justify-content-between">
+                        <li class="nav-item p-2 align-self-center">За нас</li>
+                        <li class="nav-item p-2 align-self-center">Услуги</li>
+                        <li class="nav-item p-2 align-self-center">Проекти</li>
+                        <li class="nav-item p-2 align-self-center">Кнтакт</li>
+                        <li class="nav-item fw-normal align-self-center p-2"> <button class="pushable">
+                                <span class="front">
+                                    Донирај
+                                </span>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
