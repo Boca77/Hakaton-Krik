5b914207f8ec57688d843095c558a5b98924cbdd
Boris Nacev <bnachev77@gmail.com>
2024-09-22 14:52:03 +0200

    small change

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             TeamSeeder::class,
             VolunteersSeeder::class,
             ProjectSeeder::class,
+            EventsSeeder::class
         ]);
 
         User::factory()->create([
